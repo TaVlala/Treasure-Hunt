4ebbf02e73461ca91f8f1be3705f44f00df5523f
TaVlala <dtavlalashvili@hotmail.com>
2026-05-31 00:47:48 +0400

    Delete apps directory

--- apps/mobile/babel.config.js
+++
@@ -1,8 +0,0 @@
-// Babel config for Expo SDK 52 + Expo Router.
-// babel-preset-expo handles JSX transform, React Native, and Expo Router integration.
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
