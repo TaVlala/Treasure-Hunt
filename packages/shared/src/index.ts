071f07da1041e10a64a92dceb43b9118436df5e3
TaVlala <dtavlalashvili@hotmail.com>
2026-05-31 00:48:12 +0400

    Delete packages directory

--- packages/shared/src/index.ts
+++
@@ -1,9 +0,0 @@
-// Main entry point for the shared package.
-// Import from '@treasure-hunt/shared' in any app to access these types and constants.
-
-export * from './types/user';
-export * from './types/hunt';
-export * from './types/sponsor';
-export * from './types/game';
-export * from './types/api';
-export * from './constants/game';
